c8f6af43c6070fc7ac60eafae2fc2b9e54610180
Esteban <esteban.rodriguezo@udea.edu.co>
2018-02-23 11:52:09 -0500

    Primer Ejercicio practica-1

---
+++ Ejercicio-3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int  A, B;
+    cout <<"Ingrese un Numero: ";
+    cin >> A>>B;
+
+    if(A>B){
+        cout << "El numero mayor es: "<<A<<endl;
+    }
+    else{
+            cout << "El numero mayor es: "<<B<<endl;
+              }
+}
